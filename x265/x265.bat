12ad8218f12c9a021d1c0a4dc94cfc9ea5dc574a
ImmiColoris <32576882+ImmiColoris@users.noreply.github.com>
2017-11-04 21:48:17 +0100

    2017.11.04-21:48

--- x265/x265.bat
+++ x265/x265.bat
@@ -1,1 +1,2 @@
-"vspipe.exe" "script.vpy" - --y4m | "x265-10bit.exe" --input - --y4m --input-depth 10 --profile main10 --preset veryslow --tune vcb-s++ --crf 20 --output "out.mkv"
+"vspipe.exe" "script.vpy" - --y4m | "x265-10bit.exe" --input - --y4m --input-depth 10 --profile main10 --preset veryslow --tune vcb-s++ --crf 22 --output "out.mkv"
+"vspipe.exe" "script.vpy" - --y4m | "x265-10bit.exe" --input - --y4m --input-depth 10 --profile main10 --preset veryslow --tune vcb-s++ --crf 22 --vbv-maxrate 2560 --vbv-bufsize 2560 --output "out.mkv"
